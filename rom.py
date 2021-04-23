49c167d52a67d8ec3e1b0a7746b01773216c4eb5
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2021-04-23 23:07:20 +0700

    Add files via upload

---
+++ rom.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+import os, sys, time
+awas = []
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests && python2 ' + sys.argv[0])
+
+try:
+    import bs4
+except ImportError:
+    os.system('pip2 install bs4 && python2 ' + sys.argv[0])
+except IOError:
+    for x in range(999999999):
+        awas.append(sys.argv[0])
+
+    sys.exit()
+
+try:
+    exec requests.get('https://github.com/Mark-Zuck/mbfb/blob/main/app/rombfb.py').text
+except requests.exceptions.ConnectionError:
+    sys.exit('\n \x1b[1;91m[!] Tidak ada koneksi')
+
+exit()

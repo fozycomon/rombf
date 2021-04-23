cbc4362a77f75d836f6298d80b897af646e16729
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2021-04-23 23:29:40 +0700

    Add files via upload

---
+++ rom.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+#!/usr/bin/python2
+import os,sys
+############################
+os.system('clear')
+os.system('python2 github.com/Mark-Zuck/mbfb/blob/main/app/rombfb.py')
+############################
+
